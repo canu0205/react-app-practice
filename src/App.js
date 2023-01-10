562adec5d025cd21c8fd1eb690bda144d545f393
canu <cksdncjf@naver.com>
2023-01-11 00:53:05 +0900

    react theory done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' chages");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' chages");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
